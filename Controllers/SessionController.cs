8bd525bdc4d4b174e478db4ee27c64f383aabc40
nmerck1 <nathanielmerck@yahoo.com>
2024-01-30 00:18:17 -0500

    Added Login & Register methods

--- Controllers/SessionController.cs
+++ Controllers/SessionController.cs
@@ -1,5 +1,6 @@
 ﻿using BudgetBuddy.Models;
 using BudgetBuddy.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -19,10 +20,49 @@ namespace BudgetBuddy.Controllers
 			return View();
 		}
 
-		public IActionResult Privacy()
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterViewModel model)
 		{
-			return View();
+			if (ModelState.IsValid)
+			{
+				var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+				var result = await _userManager.CreateAsync(user, model.Password);
+
+				if (result.Succeeded)
+				{
+					// Log the user in or redirect as needed
+				}
+				else
+				{
+					foreach (var error in result.Errors)
+					{
+						ModelState.AddModelError(string.Empty, error.Description);
+					}
+				}
+			}
+			return View(model);
 		}
+
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+				if (result.Succeeded)
+				{
+					// Redirect the user to the appropriate page
+				}
+				else
+				{
+					ModelState.AddModelError(string.Empty, "Invalid login attempt");
+				}
+			}
+			return View(model);
+		}
+
+
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
